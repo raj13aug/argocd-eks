27bcafe1d3651a5b5277d05c64a83527f5db8259
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2022-10-27 21:15:15 +0530

    Update argocd.tf

--- argocd.tf
+++ argocd.tf
@@ -1,3 +1,5 @@
+#https://artifacthub.io/packages/helm/argo/argo-cd/3.1.2
+
 module "iam_assumable_role_oidc" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
   version = "5.2.0"
@@ -27,32 +29,7 @@ resource "helm_release" "argocd" {
   ## Server params
   set {
     name  = "server.service.type"
-    value = "ClusterIP"
-  }
-
-  set { # Enable Horizontal Pod Autoscaler () for the Argo CD server
-    name  = "server.autoscaling.enabled"
-    value = "true"
-  }
-
-  set { # Maximum number of replicas for the Argo CD server HPA
-    name  = "server.autoscaling.maxReplicas"
-    value = "1"
-  }
-
-  set { # Minimum number of replicas for the Argo CD server HPA
-    name  = "server.autoscaling.minReplicas"
-    value = "1"
-  }
-
-  set { # Average CPU utilization percentage for the Argo CD server HPA
-    name  = "server.autoscaling.targetCPUUtilizationPercentage"
-    value = "60"
-  }
-
-  set { # Average memory utilization percentage for the Argo CD server HPA
-    name  = "server.autoscaling.targetMemoryUtilizationPercentage"
-    value = "70"
+    value = "LoadBalancer"
   }
 
   set { # Manage Argo CD configmap (Declarative Setup)
@@ -63,26 +40,6 @@ resource "helm_release" "argocd" {
   set { # Argo CD server name
     name  = "server.name"
     value = "argocd-server"
-  }
-
-  set { # Enable an ingress resource for the Argo CD server for dedicated gRPC-ingress
-    name  = "server.ingressGrpc.enabled"
-    value = "true"
-  }
-
-  set { # List of ingress hosts for dedicated gRPC-ingress
-    name  = "server.ingressGrpc.hosts"
-    value = "argocd.esp.mgmt"
-  }
-
-  set { # Defines which ingress controller will implement the resource gRPC-ingress
-    name  = "server.ingressGrpc.ingressClassName"
-    value = "alb"
-  }
-
-  set { # Setup up gRPC ingress to work with an AWS ALB
-    name  = "server.ingressGrpc.isAWSALB"
-    value = "true"
   }
 
   set { # Annotations applied to created service account
