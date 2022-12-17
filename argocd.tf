f4533909bb9ec3cae9aa26cfa5045738c2f3db1f
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2022-12-17 12:59:30 +0530

    Update argocd.tf

--- argocd.tf
+++ argocd.tf
@@ -19,13 +19,14 @@ data "terraform_remote_state" "kubeconfig_file" {
 }
 
 
+
 module "iam_assumable_role_oidc" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
   version = "5.2.0"
 
   create_role                   = true
   role_name                     = "k8s-argocd-admin"
-  provider_url                  = replace(data.terraform_remote_state.kubeconfig_file.outputs.cluster_oidc_issuer_url, "https://", "")   #module.eks.cluster_oidc_issuer_url
+  provider_url                  = replace(data.terraform_remote_state.kubeconfig_file.outputs.cluster_oidc_issuer_url, "https://", "")
   role_policy_arns              = []
   oidc_fully_qualified_subjects = ["system:serviceaccount:${var.argocd_k8s_namespace}:argocd-server", "system:serviceaccount:${var.argocd_k8s_namespace}:argocd-application-controller"]
 }
